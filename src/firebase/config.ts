60c356e2476575eee1a5e4112f05bb2eef2ed1c6
Moritz Meyer <moritz@organicconcepts.de>
2026-01-29 22:54:10 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3635404335-34316",
+  "appId": "1:767128293206:web:869e9f54a4056ee5da50f7",
+  "apiKey": "AIzaSyCDhg_-0KM4YhWl0DMUQZB-tNjcThgwnUA",
+  "authDomain": "studio-3635404335-34316.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "767128293206"
+};
