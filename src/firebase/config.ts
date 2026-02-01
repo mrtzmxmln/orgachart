3ffee425c9aa6c9c304997237b791ffca8ce3fbe
Moritz Meyer <moritz@organicconcepts.de>
2026-02-01 21:43:32 +0000

    Wenn ich mich über meine Domain mit google verbinde, soll oben auch mein

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,7 +2,7 @@ export const firebaseConfig = {
   "projectId": "studio-3635404335-34316",
   "appId": "1:767128293206:web:869e9f54a4056ee5da50f7",
   "apiKey": "AIzaSyCDhg_-0KM4YhWl0DMUQZB-tNjcThgwnUA",
-  "authDomain": "studio-3635404335-34316.firebaseapp.com",
+  "authDomain": "orgachart.de",
   "measurementId": "",
   "messagingSenderId": "767128293206"
 };
